40aec5a23de196ec78a4e218b343693bc53ab3ce
otod <olivertod11@yahoo.de>
2020-07-20 16:12:25 +0200

    generated deployables

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Stat, StatReduced} from '../models/stat';
-import {environment} from '../../environments/environment';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://srps1w5wok.execute-api.us-east-2.amazonaws.com/default'
 };
